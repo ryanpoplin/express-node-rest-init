e6daaf816be880527376cb969ca3a5033ad28cb3
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2016-03-11 17:28:16 -0500

    concluded that gulp is create, but it's for client packing

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,7 @@ const gulp = require("gulp"),
 gulp.task("js", function() { // get our JS files task
 	// gulp.src(can take an array of file paths)
 	// ** && * in the file path???
-
-	// !!! just testing it out, don't do this for node.js program...
+	// just testing, this is not useful for server side code, this is because these files are not being downloaded over the network, so the performance gains to the v8 engine are very minimal compared to sending and reading minified, uglified, and concatenated files over the network to clients
 	gulp.src(["./server.js", "./app/**/*.js", "./config/*.js", "./config/env/development.js"]) // source
 	.pipe(concat("scripts.js")) // stream them to concat plugin
 	.pipe(gulp.dest("./build/")) // dir
