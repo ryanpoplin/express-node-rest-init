9aace2330893f7ee73cf92e41616ebc3cd21c70b
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2016-03-14 23:14:16 -0400

    remove crypto trim from user model

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
 	// edit this!
 	name: {type: String, unique: true, required: true}, // all usernames must be unique
 	email: {type: String, unique: true, required: true, index: true}, // 2ndary index
-	username: {type: String, unique: true, required: true, trim: true}, 
+	username: {type: String, unique: true, required: true}, 
 	password: {type: String, unique: false, required: true, /*provider: String, providerId: String, providerData: {},*/ tasks: {}}
 });
 
