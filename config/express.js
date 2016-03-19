b153d39c8dae27f91ff5b223dcda8b9ef2b8b2ef
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2016-03-19 15:14:21 -0400

    solid REST setup

--- config/express.js
+++ config/express.js
@@ -2,23 +2,26 @@
 
 const express = require("express"), // grab our express module
 	  config = require("./config"), // grab our config file
-	  bodyParser = require("body-parser"); // grab our body-parser module
+	  bodyParser = require("body-parser"), // grab our body-parser module
+	  morgan = require("morgan");
 
 module.exports = function() {
 	const app = express(); // create an instance of express (express app)
 
 	// add middleware 
-	app.use(bodyParser.urlencoded({
+	app.use(bodyParser.urlencoded({ // use body-parser to grab data from POST requests
 		extended: true // encode our url's...
 	}));
 	app.use(bodyParser.json()); // expect JSON data format
-	// CORS (TODO: research this a lot more...)
-	app.use(function(req, res, next) {
+	// CORS settings allows our API to be accessed from bascially anywhere...
+	app.use(function(req, res, next) { // CORS requests
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
 		next();
 	});
+
+	app.use(morgan("dev")); // log all requests to the console...
 
 	app.use(express.static("./public")); // expect all static files to come from 
 										 // ./public dir
@@ -27,17 +30,17 @@ module.exports = function() {
 	app.set("view engine", "ejs"); // set template engine: ejs module
 
 	// express.Router() is like mini-application on its own; only with routing functionalities though...
-	const indexRouter = express.Router();
 	// load in our index routes
 	require("../app/routes/index.server.routes")(indexRouter);
+	const indexRouter = express.Router();
 
-	const usersRouter = express.Router();
 	// load in our users routes
 	// NOTE: make sure postman content-type is set to JSON
 	require("../app/routes/users.server.routes")(usersRouter); // grab the user.server.routes function
+	const usersRouter = express.Router();
 	
-	app.use("/index", indexRouter); // 
-	app.use("/users", usersRouter); // 
+	app.use("/api/index", indexRouter); // register the routes with our custom routers 
+	app.use("/api/users", usersRouter); 
 
 	// return the express app for the server.js file
 	return app;
