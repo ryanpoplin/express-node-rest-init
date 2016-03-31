f7d0586e409ec64d2a0ffa64ea874306ff9b5348
Byrdann Fox <byrdannfox@gmail.com>
2016-03-31 13:22:02 -0400

    remove and add some files

--- manipulate.json.spec.js
+++ manipulate.json.spec.js
@@ -1,57 +1,57 @@
-"use strict";
+// "use strict";
 
-const request = require("superagent");
-const expect = require("chai").expect;
+// const request = require("superagent");
+// const expect = require("chai").expect;
 
-const remove = require("./remove.user.data");
-const alter = require("./alter.user.data");
+// const remove = require("./remove.user.data");
+// const alter = require("./alter.user.data");
 
-describe("express rest api server", function() {
+// describe("express rest api server", function() {
 	     
-	it("should pass basic fault test", function(done) {
-		request.get("http://localhost:8080/api/users/list").end(function(err, res) {
-			expect(err).to.equal(null);
-			expect(res.status).to.equal(200);
-			expect(res.body["Best Users"]["Current Best Users"]).to.have.length.above(0);
-			done();
-		});
-	});
+// 	it("should pass basic fault test", function(done) {
+// 		request.get("http://localhost:8080/api/users/list").end(function(err, res) {
+// 			expect(err).to.equal(null);
+// 			expect(res.status).to.equal(200);
+// 			expect(res.body["Best Users"]["Current Best Users"]).to.have.length.above(0);
+// 			done();
+// 		});
+// 	});
 
-	it("should confirm JSON schema output expectations", function(done) {
-		request.get("http://localhost:8080/api/users/list").end(function(err, res) {
-			const usersArr = res.body["Best Users"]["Current Best Users"];
-			remove(usersArr);
-			alter(usersArr);
-			usersArr.forEach(function(user) {
-				Object.keys(user).forEach(function(key) {
-					switch (key) {
-						case "newProp":
-							expect(key).to.be.a("string");
-							break;
-						case "quality":
-							expect(key).to.be.a("string");
-							break;
-						case "username":
-							expect(key).to.be.a("string");
-							break;
-						case "name":
-							expect(key).to.be.a("string");
-							break;
-						case "arryOfValues":
-							// should never be expected to run
-							expect(key).to.be.a("asdfjkl;");
-							break;
-						case "test":
-							// should never be expected to run
-							expect(key).to.be.a("asdfjkl;");
-							break;
-						default:
-							break;
-					}
-				});
-			});
-			done();
-		});
-	});
+// 	it("should confirm JSON schema output expectations", function(done) {
+// 		request.get("http://localhost:8080/api/users/list").end(function(err, res) {
+// 			const usersArr = res.body["Best Users"]["Current Best Users"];
+// 			remove(usersArr);
+// 			alter(usersArr);
+// 			usersArr.forEach(function(user) {
+// 				Object.keys(user).forEach(function(key) {
+// 					switch (key) {
+// 						case "newProp":
+// 							expect(key).to.be.a("string");
+// 							break;
+// 						case "quality":
+// 							expect(key).to.be.a("string");
+// 							break;
+// 						case "username":
+// 							expect(key).to.be.a("string");
+// 							break;
+// 						case "name":
+// 							expect(key).to.be.a("string");
+// 							break;
+// 						case "arryOfValues":
+// 							// should never be expected to run
+// 							expect(key).to.be.a("asdfjkl;");
+// 							break;
+// 						case "test":
+// 							// should never be expected to run
+// 							expect(key).to.be.a("asdfjkl;");
+// 							break;
+// 						default:
+// 							break;
+// 					}
+// 				});
+// 			});
+// 			done();
+// 		});
+// 	});
 
-});
+// });
--- remove.user.data.js
+++ remove.user.data.js
@@ -1,27 +1,27 @@
 "use strict";
 
-// TODO: just remove these guys via mongoose
-// for 3rd-party
-module.exports = function removePropertiesFromUsersArray(users) {
-	users.forEach(function(user) {
-		Object.keys(user).forEach(function(key) {
-			switch (key) {
-				case "_id":
-					delete user["_id"];
-					break;
-				case "__v":
-					delete user["__v"];
-					break;
-				case "password":
-					delete user["password"];
-					break;
-				case "email":
-					delete user["email"];
-					break;
-				default:
-					break;
-			}
-		});
-	});
-	return users;
-}
+// // TODO: just remove these guys via mongoose
+// // for 3rd-party
+// module.exports = function removePropertiesFromUsersArray(users) {
+// 	users.forEach(function(user) {
+// 		Object.keys(user).forEach(function(key) {
+// 			switch (key) {
+// 				case "_id":
+// 					delete user["_id"];
+// 					break;
+// 				case "__v":
+// 					delete user["__v"];
+// 					break;
+// 				case "password":
+// 					delete user["password"];
+// 					break;
+// 				case "email":
+// 					delete user["email"];
+// 					break;
+// 				default:
+// 					break;
+// 			}
+// 		});
+// 	});
+// 	return users;
+// }
